954445f770011f554864a09a60a96284a3557606
BrayanMnz <brayanmnz@hotmail.com>
2022-07-04 23:25:58 -0400

    SpringInAction : Initializing project.

---
+++ Spring/src/test/java/brayanmnz/springinaction/SpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package brayanmnz.springinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
